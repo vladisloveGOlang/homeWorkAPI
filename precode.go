5b6ed69926e906d03e14e4c83d590e0eadfaabea
Vladislav Kudelnikov <vladkudelnikov96@gmail.com>
2024-10-24 12:17:13 +0300

    CRUD

--- precode.go
+++ precode.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -41,12 +43,101 @@ var tasks = map[string]Task{
 
 // Ниже напишите обработчики для каждого эндпоинта
 // ...
+func getHandler(w http.ResponseWriter, r *http.Request) {
+
+	bytes, err := json.Marshal(tasks)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	_, err = w.Write(bytes)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	bytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
+	var task Task
+	err = json.Unmarshal(bytes, &task)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
+	tasks[task.ID] = task
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	bytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var task Task
+
+	err = json.Unmarshal(bytes, &task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if tasks[task.ID].ID == "" {
+		w.WriteHeader(http.StatusConflict)
+		w.Write([]byte("Задачи с таким номером не существует"))
+		return
+	}
+
+	delete(tasks, task.ID)
+	w.WriteHeader(http.StatusAccepted)
+
+}
+
+func putchHandler(w http.ResponseWriter, r *http.Request) {
+	bytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var task Task
+
+	err = json.Unmarshal(bytes, &task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tasks[task.ID] = task
+
+	w.WriteHeader(http.StatusOK)
+
+}
 
 func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getHandler)
+	r.Post("/tasks", postHandler)
+	r.Delete("/tasks", deleteHandler)
+	r.Patch("/tasks", putchHandler)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
